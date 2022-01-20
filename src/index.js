f60173779312012dbcb1aed2f2911c5f673a4d59
EjayCinchez <edgarjaycinchez@gmail.com>
2022-01-20 17:01:49 +0800

    Make use of props and create Task component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 
-function TasksList(){
-
-	return (
-		<ul>
-			<li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
-			<li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
-			<li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
-		</ul>
+function Task(props){
+	return(
+		<li className="tasks-item">{props.taskName}</li>
 	);
 }
 
+function TasksList(){
+	return (
+		<ul>
+			<Task taskName="Follow Edukasyon.ph on Facebook."/>
+			<Task taskName="Follow AWS Siklab Pilipinas on Facebook."/>
+			<Task taskName="Follow Zuitt Coding Bootcamp on Facebook."/>
+		</ul>
+	);
+}
 
 function App(){
 	
