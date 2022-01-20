65ee193901b8f61ceda86f0176034448dbfea876
EjayCinchez <edgarjaycinchez@gmail.com>
2022-01-20 16:48:10 +0800

    Create a new TasksList component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 
+function TasksList(){
+
+	return (
+		<ul>
+			<li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+			<li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+			<li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+		</ul>
+	);
+}
+
+
 function App(){
 	
 	const paraStyle = {
@@ -13,12 +25,7 @@ function App(){
 		<header className="app-header">
 			<h1>My Amazing ToDo-List App</h1>
 			<p style={paraStyle}>The most simple and amazing todo-list React app.</p>
-
-			<ul>
-				<li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
-				<li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
-				<li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
-			</ul>
+			<TasksList/>
 		</header>
 	</div>
 	);
