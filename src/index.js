df3e8bebd9b05b881024494753c42583a03c3527
EjayCinchez <edgarjaycinchez@gmail.com>
2022-01-21 14:00:22 +0800

    Integrated component with array

--- src/index.js
+++ src/index.js
@@ -10,17 +10,24 @@ function Task(props){
 }
 
 function TasksList(){
+	const taskItemList = [
+		"Follow Edukasyon.ph on Facebook.",
+		"Follow AWS Siklab Pilipinas on Facebook.",
+		"Follow Zuitt Coding Bootcamp on Facebook."
+	];
 	return (
+		<div>
+		<input />
 		<ul>
-			<Task taskName="Follow Edukasyon.ph on Facebook."/>
-			<Task taskName="Follow AWS Siklab Pilipinas on Facebook."/>
-			<Task taskName="Follow Zuitt Coding Bootcamp on Facebook."/>
+			{taskItemList.map((task, index) => {
+				return <Task key={index} taskName={task}/>
+			})}
 		</ul>
+		</div>
 	);
 }
 
 function App(){
-	
 	const paraStyle = {
 		"marginBottom": "15px"
 	};
